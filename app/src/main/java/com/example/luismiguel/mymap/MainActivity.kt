6f738cd34984ec232b4077021936d0bc68c88434
luismiguel <luismikg@gmail.com>
2018-05-09 21:26:32 -0500

    Se insertaron 2 opciones en la barra de la aplicacion Se esta trabajando en las preferencias

--- app/src/main/java/com/example/luismiguel/mymap/MainActivity.kt
+++ app/src/main/java/com/example/luismiguel/mymap/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.luismiguel.mymap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menuInflater.inflate(R.menu.configuration_places, menu)
+        this.menuInflater.inflate(R.menu.configuration_buttons, menu)
+        return true
     }
 }
