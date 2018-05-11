11fc6773d46bcc1608f38bf6131d968b3fea81ea
desde MAC <luismikg@gmail.com>
2018-05-11 00:45:12 -0500

    Se presenta las preferencias del ususario respecto al numero de botones Se presenta las preferencias del ususario respecto al intervalo de tiempo para actualizar su coordenada Se presenta un layaout para smartphone y otro para tableta

--- app/src/main/java/com/example/luismiguel/mymap/MainActivity.kt
+++ app/src/main/java/com/example/luismiguel/mymap/MainActivity.kt
@@ -1,28 +1,89 @@
 package com.example.luismiguel.mymap
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var isSmarthone: Boolean = true
+    private var preferencesChanged: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //https://www.skyscanner.es/noticias/10-lugares-espectaculares-del-mundo-que-deberias-visitar
+        //PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener( preferencesChangeListener )
+
+        val screenSize:Int = resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK
+        if( screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE ) {
+            this.isSmarthone = false
+        }
+
+        if( this.isSmarthone ){
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }else{
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        this.menuInflater.inflate(R.menu.configuration_places, menu)
-        this.menuInflater.inflate(R.menu.configuration_buttons, menu)
+        if( this.isSmarthone ){
+            //this.menuInflater.inflate(R.menu.configuration_places, menu)
+            this.menuInflater.inflate(R.menu.configuration_buttons, menu)
+        }
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        var preferencesIntent:Intent = Intent(this, PreferencesPlacesConfigurations::class.java)
+        val preferencesIntent:Intent = Intent(this, PreferencesPlacesConfigurations::class.java)
         startActivity( preferencesIntent )
 
         return super.onOptionsItemSelected(item)
     }
+
+
+    // Listener for changes to the app's SharedPreferences.
+    /*private val preferencesChangeListener = OnSharedPreferenceChangeListener { sharedPreferences, key ->
+        // Called when the user changes the app's preferences.
+        preferencesChanged = true  // User changed app settings.
+
+        val quizFragment = supportFragmentManager.findFragmentById(R.id.quizFragment) as MainActivityFragment
+
+        if (key == CHOICES) {
+            // Number of choices to display changed.
+            quizFragment.updateGuessRows(sharedPreferences)
+            quizFragment.resetQuiz()
+        } else if (key == REGIONS) {
+            // Regions to include changed.
+            val regions = sharedPreferences.getStringSet(REGIONS, null)
+
+            if (regions != null && regions.size > 0) {
+                quizFragment.updateRegions(sharedPreferences)
+                quizFragment.resetQuiz()
+            } else {
+                // Must select one region-set, set North America as default.
+                val editor = sharedPreferences.edit()
+                regions!!.add(getString(R.string.default_region))
+                editor.putStringSet(REGIONS, regions)
+                editor.apply()
+
+                Toast.makeText(this@MainActivity,
+                        R.string.default_region_message,
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+        Toast.makeText(this@MainActivity,
+                R.string.restarting_quiz,
+                Toast.LENGTH_SHORT).show()
+    }*/
 }
